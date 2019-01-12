1fd32f9df7f9394e92059d5ec7dbd9d8e4021159
sachin <sachinworkk@gmail.com>
2019-01-12 21:34:19 +0545

    Added models and controller

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -335,6 +335,7 @@ class ComposerStaticInit1146f5486dafa95ba243e4dd169d75db
         'App\\Http\\Middleware\\TrimStrings' => __DIR__ . '/../..' . '/app/Http/Middleware/TrimStrings.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Model\\Posts' => __DIR__ . '/../..' . '/app/Model/Posts.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
